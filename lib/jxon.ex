88f6d0d38a32ae06ad4c120da66eca692c2bd6a2
Dirham <dirhamsistem@gmail.com>
2024-07-28 19:23:32 +0800

    initial project

---
+++ lib/jxon.ex
@@ -0,0 +1,18 @@
+defmodule Jxon do
+  @moduledoc """
+  Documentation for `Jxon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jxon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jxon_test.exs
@@ -0,0 +1,8 @@
+defmodule JxonTest do
+  use ExUnit.Case
+  doctest Jxon
+
+  test "greets the world" do
+    assert Jxon.hello() == :world
+  end
+end
